c8d79d33ec78dbeed071bd8af5499a0bf5528e51
Tomas Sæbu <tomas.saebu@eika.no>
2018-10-15 15:27:35 +0200

    Updates

---
+++ precache-manifest.942e7842fdda39fc4b49e305e6465830.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c067511d1c4611b3f2719506f9f81956",
+    "url": "/saebu/static/media/krokodille.c067511d.png"
+  },
+  {
+    "revision": "30fdde5d144fb9a730330ecc015b2fa8",
+    "url": "/saebu/static/media/hare.30fdde5d.png"
+  },
+  {
+    "revision": "70bcda262b00a215410853c472f4ce3d",
+    "url": "/saebu/static/media/flodhest.70bcda26.png"
+  },
+  {
+    "revision": "d7eac405105fd20db9d15cc28866a922",
+    "url": "/saebu/static/media/and.d7eac405.png"
+  },
+  {
+    "revision": "01d2d2fd14974fe616ee",
+    "url": "/saebu/static/js/runtime~main.01d2d2fd.js"
+  },
+  {
+    "revision": "2cac891d16ffa95746f1",
+    "url": "/saebu/static/js/main.2cac891d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/saebu/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2cac891d16ffa95746f1",
+    "url": "/saebu/static/css/main.d73ab120.chunk.css"
+  },
+  {
+    "revision": "0078b0f8395d58114bbc9fd5002b06eb",
+    "url": "/saebu/index.html"
+  }
+];
