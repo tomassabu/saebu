c5b3d7c2c2f1e369baafb5c60e41752661623cf9
Tomas Stein Sæbu <tomas.saebu@solidsquare.no>
2018-10-15 22:20:21 +0200

    la til flere bilder og viser de nå i grid

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
-import hare from './bilder/hare.png';
+import tegninger from './bilder';
+import tomas from './bilder/tomas1.png';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
 
-<div className="container-fluid bg-1 text-center">
-  <h3>morn morn morn jeg heter tomas</h3>
-  <img src={require('./bilder/flodhest.png')} className="img-responsive img-circle margin" alt="Forsidebilde" width="350" height="350" />
-</div>
+        <div className="container-fluid bg-1 text-center">
+          <h3>morn morn morn jeg heter tomas</h3>
+          <img src={tomas} className="img-responsive img-circle margin" alt="Forsidebilde" width="350" height="350" />
+        </div>
 
-<div className="container-fluid bg-2 text-center">
-  <h4 className="margin">Bilder</h4>
-  <p>Jeg har tegnet masse bilder, de dukker kanskje opp her. </p>
-</div>
+        <div className="container-fluid bg-2 text-center">
+          <h4 className="margin">Bilder</h4>
+          <p>Jeg har tegnet masse bilder. De kan du finne under.</p>
+          <p>Jeg liker å bruke tegneprogrammet paint når jeg tenger.</p>
+          <p>Denne nettsiden er laget i React.</p>
+        </div>
 
-<div className="container-fluid bg-3 text-center">
-  <div className="row">
-    <div className="col-sm-4">
-      <p>Bilde av en harepus.</p>
-      <img src={hare} className="img-responsive margin" alt="Image2" />
-    </div>
-    <div className="col-sm-4">
-      <p>Bilde av en krokodille som leker med en badeball.</p>
-      <img src={require('./bilder/krokodille.png')} className="img-responsive margin" alt="Image3" />
-    </div>
-    <div className="col-sm-4">
-      <p>Bilde av en and som står foran en bygning som er, for eksempel, en pengebinge.</p>
-      <img src={require('./bilder/and.png')} className="img-responsive margin" alt="Image4" />
-    </div>
-  </div>
-</div>
+        <div className="container-fluid bg-3 text-center gallery-grid">
+
+          {tegninger.map(t => (
+              <div key={t.id}>
+                {/*<p>{t.navn}</p>*/}
+                <img src={t.bilde} className="img-responsive margin gridTegning" alt={t.navn} />
+              {/* <p>{t.beskrivelse}</p> */}
+              </div>
+          )
+          )}
+        </div>
 
         <footer className="container-fluid bg-4 text-center">
           <p> Mer kommer </p>
---
+++ src/bilder.js
@@ -0,0 +1,213 @@
+import flodhest from "./bilder/flodhest.png";
+import harepus from "./bilder/harepus.png";
+import helene from "./bilder/helene.png";
+// import illidan from "./bilder/illidan.png";
+import jagerfly from "./bilder/jagerfly.png";
+import krokodille from "./bilder/krokodille.png";
+import pirat from "./bilder/pirat.png";
+import prinsesse from "./bilder/prinsesse.png";
+import ridder from "./bilder/ridder.png";
+import robot from "./bilder/robot.png";
+import romskip from "./bilder/romskip.png";
+import romvesen from "./bilder/romvesen.png";
+import romvesine from "./bilder/romvesine.png";
+import sjakktrekk from "./bilder/sjakktrekk.png";
+import sjonglor from "./bilder/sjonglor.png";
+import snomann from "./bilder/snomann.png";
+import strandmann from "./bilder/strandmann.png";
+//import tomas1 from "./bilder/tomas1.png";
+import traktor from "./bilder/traktor.png";
+import viking from "./bilder/viking.png";
+import xul from "./bilder/xul.png";
+import bamse from "./bilder/bamse.png";
+import bil from "./bilder/bil.png";
+import cowboy from "./bilder/cowboy.png";
+import donald1 from "./bilder/donald1.png";
+import drage1 from "./bilder/drage1.png";
+//import drage4 from "./bilder/drage4.png";
+import elefant from "./bilder/elefant.png";
+import enhjorning from "./bilder/enhjorning.png";
+import fange from "./bilder/fange.png";
+
+const tegninger = [
+/*  {
+    id: 0,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Tomas",
+    bilde: tomas1
+  },*/
+  {
+    id: 1,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Fange",
+    bilde: fange
+  },
+  {
+    id: 2,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Enhjorning",
+    bilde: enhjorning
+  },
+  {
+    id: 3,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Ridder",
+    bilde: ridder
+  },
+  {
+    id: 4,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Skjørøver",
+    bilde: pirat
+  },
+  {
+    id: 5,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "prinsesse",
+    bilde: prinsesse
+  },
+  {
+    id: 6,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Robot",
+    bilde: robot
+  },
+  {
+    id: 7,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Romskip",
+    bilde: romskip
+  },
+  {
+    id: 8,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Romvesen",
+    bilde: romvesen
+  },
+  {
+    id: 9,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Romvesine",
+    bilde: romvesine
+  },
+  {
+    id: 10,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Sjakktrekk",
+    bilde: sjakktrekk
+  },
+  {
+    id: 11,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Sjonglør",
+    bilde: sjonglor
+  },
+  {
+    id: 12,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Snømann",
+    bilde: snomann
+  },
+  {
+    id: 13,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Ligger på stranda",
+    bilde: strandmann
+  },
+  {
+    id: 14,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "traktor",
+    bilde: traktor
+  },
+  {
+    id: 15,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Vikinger",
+    bilde: viking
+  },
+  {
+    id: 16,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Den grimme ripern",
+    bilde: xul
+  },
+  {
+    id: 17,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Bamse og leker",
+    bilde: bamse
+  },
+  {
+    id: 18,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Cowboy",
+    bilde: cowboy
+  },
+  {
+    id: 19,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "En And",
+    bilde: donald1
+  },
+  {
+    id: 20,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Drage",
+    bilde: drage1
+  },
+  {
+    id: 21,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "elefant",
+    bilde: elefant
+  },
+  {
+    id: 22,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Bil",
+    bilde: bil
+  },
+  {
+    id: 23,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Krigerdame",
+    bilde: helene
+  },
+  {
+    id: 24,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Krokodille",
+    bilde: krokodille
+  },
+  {
+    id: 25,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Jagerfly",
+    bilde: jagerfly
+  },
+  {
+    id: 26,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Flodhest",
+    bilde: flodhest
+  },
+  {
+    id: 27,
+    beskrivelse: "Bilde av meg som sitter vel pulten min.",
+    navn: "Harepus",
+    bilde: harepus
+  }
+];
+
+export default tegninger;
+
+/*
+Mal for nye Bilder
+{
+  id: ,
+  beskrivelse: ,
+  navn: ,
+  bilde:
+}
+*/
